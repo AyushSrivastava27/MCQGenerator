b1e1e89338900a43bd5771620ec826108ee54062
Ayush <ayushsrivastava8427@gmail.com>
2024-05-20 19:43:51 +0530

    updated files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 
 setup(
-    name='mcqgenrator',
+    name='mcqgenerator',
     version='0.0.1',
     author='Ayush Srivastava',
     author_email='ayushsrivastava@gmail.com',
